78f4962c560cf5a40c647227b033ef27ae45d796
circularuins <natsuhiko.wake@gmail.com>
2018-01-21 18:49:07 +0900

    mockitoで非同期処理をテスト

--- app/src/androidTest/java/com/circularuins/kotlinsample/MainActivityTest.kt
+++ app/src/androidTest/java/com/circularuins/kotlinsample/MainActivityTest.kt
@@ -1,17 +1,22 @@
 package com.circularuins.kotlinsample
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.view.View
+import com.circularuins.kotlinsample.client.ArticleClient
+import io.reactivex.Observable
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.*
 
 
 /**
@@ -34,4 +39,17 @@ class MainActivityTest {
     }
 
     fun isNotDisplayed(): Matcher<View> = not(isDisplayed())
+
+    @Test
+    fun `検索ボタンがタップされたら、入力されたクエリ文字列で記事検索APIを叩くこと`() {
+        val articleClient = mock(ArticleClient::class.java).apply {
+            `when`(search("user:circularuins")).thenReturn(Observable.just(listOf()))
+        }
+        activityTestRule.activity.articleClient = articleClient
+
+        onView(withId(R.id.query_edit_text)).perform(typeText("user:circularuins"))
+        onView(withId(R.id.search_button)).perform(click())
+
+        verify(articleClient).search("user:circularuins")
+    }
 }
