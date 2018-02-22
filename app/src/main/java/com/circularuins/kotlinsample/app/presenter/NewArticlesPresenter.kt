4c264eea915719222e1c4d835f0634f68c5e24da
circularuins <natsuhiko.wake@gmail.com>
2018-02-23 00:01:34 +0900

    Refactor Observer

--- app/src/main/java/com/circularuins/kotlinsample/app/presenter/NewArticlesPresenter.kt
+++ app/src/main/java/com/circularuins/kotlinsample/app/presenter/NewArticlesPresenter.kt
@@ -6,8 +6,6 @@ import com.circularuins.kotlinsample.domain.model.User
 import com.circularuins.kotlinsample.domain.repository.ArticlesRepository
 import com.circularuins.kotlinsample.domain.usecase.ArticlesViewUseCase
 import com.trello.rxlifecycle2.LifecycleTransformer
-import io.reactivex.Observer
-import io.reactivex.disposables.Disposable
 
 /**
  * Created by circularuins on 2018/02/18.
@@ -24,23 +22,14 @@ class NewArticlesPresenter(private val view: NewArticlesContract.View,
 
         val useCase = ArticlesViewUseCase(articlesRepository)
         useCase.getNews()
+                .doAfterTerminate {
+                    view.hideProgress()
+                }
                 .compose(transformer)
-                .subscribe(object  : Observer<List<Article>> {
-                    override fun onComplete() {
-                        view.hideProgress()
-                    }
-
-                    override fun onSubscribe(d: Disposable) {
-                        // NOP
-                    }
-
-                    override fun onNext(t: List<Article>) {
-                        view.setList(t)
-                    }
-
-                    override fun onError(e: Throwable) {
-                        view.showError(e)
-                    }
+                .subscribe({
+                    view.setList(it)
+                }, {
+                    view.showError(it)
                 })
     }
 
--- app/src/main/java/com/circularuins/kotlinsample/app/presenter/UsersArticlesPresenter.kt
+++ app/src/main/java/com/circularuins/kotlinsample/app/presenter/UsersArticlesPresenter.kt
@@ -5,8 +5,6 @@ import com.circularuins.kotlinsample.domain.model.Article
 import com.circularuins.kotlinsample.domain.repository.ArticlesRepository
 import com.circularuins.kotlinsample.domain.usecase.ArticlesViewUseCase
 import com.trello.rxlifecycle2.LifecycleTransformer
-import io.reactivex.Observer
-import io.reactivex.disposables.Disposable
 
 /**
  * Created by circularuins on 2018/02/18.
@@ -24,23 +22,14 @@ class UsersArticlesPresenter(private val view: UsersArticlesContract.View,
 
         val useCase = ArticlesViewUseCase(articlesRepository)
         useCase.getArticles(queryId)
+                .doAfterTerminate {
+                    view.hideProgress()
+                }
                 .compose(transformer)
-                .subscribe(object  : Observer<List<Article>> {
-                    override fun onComplete() {
-                        view.hideProgress()
-                    }
-
-                    override fun onSubscribe(d: Disposable) {
-                        // NOP
-                    }
-
-                    override fun onNext(t: List<Article>) {
-                        view.setList(t)
-                    }
-
-                    override fun onError(e: Throwable) {
-                        view.showError(e)
-                    }
+                .subscribe({
+                    view.setList(it)
+                }, {
+                    view.showError(it)
                 })
     }
 
